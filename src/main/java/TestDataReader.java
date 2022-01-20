1ec38ca77d11035df8cb624d1d6330ee2f507472
Angele <angele.jasinskaite@gmail.com>
2022-01-20 09:13:10 +0200

    Initial commit

---
+++ src/main/java/TestDataReader.java
@@ -0,0 +1,46 @@
+package com.proto.util;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestDataReader {
+
+    static Properties properties;
+    public static Properties getTestData() throws IOException {
+       // FileInputStream fp = new FileInputStream("src\\main\\resources\\testdata.properties");
+        //Properties properties = new Properties();
+       // properties.load(fp);
+        if (properties == null) {
+            InputStream stream = TestDataReader.class.getResourceAsStream("/testdata.properties");
+            properties = new Properties();
+            properties.load(stream);
+        }
+        return properties;
+    }
+
+    public static String getUrl() throws IOException {
+        return getTestData().getProperty("baseUrl");
+    }
+
+    public static String getBrowser() throws IOException {
+        return getTestData().getProperty("browser");
+    }
+
+    public static String getUsername() throws IOException {
+        return getTestData().getProperty("username");
+    }
+
+    public static String getPassword() throws IOException {
+        return getTestData().getProperty("password");
+    }
+
+    public static String getEmail() throws IOException {
+        return getTestData().getProperty("email");
+    }
+
+    public static String getBirthday() throws IOException {
+        return getTestData().getProperty("birthday");
+    }
+
+}
---
+++ src/main/java/com/proto/base/BaseTest.java
@@ -0,0 +1,46 @@
+package com.proto.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static com.proto.util.TestDataReader.*;
+public class BaseTest {
+    public WebDriver driver;
+    //DesiredCapabilities dc;
+
+    @BeforeTest
+    public void setUp() throws IOException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+        //String browser = getBrowser();
+        String baseUrl = getUrl();
+        //String nodeUrl = getNode();
+        //dc = new DesiredCapabilities();
+        //dc.setPlatform(Platform.WINDOWS);
+        //dc.setBrowserName(browser);
+        //driver = new RemoteWebDriver(new URL(nodeUrl), dc);
+        //System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        //driver = new ChromeDriver();
+        driver.get(baseUrl);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+    }
+    @AfterTest
+    public void tearDown() {
+        //driver.close();
+    }
+}
